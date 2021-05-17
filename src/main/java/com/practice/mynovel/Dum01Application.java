bbfe957e856268573d359eb6c96a44629d10b011
Pachara <pacharabank1@gmail.com>
2021-05-17 22:41:40 +0700

    Project starter

---
+++ src/main/java/com/practice/mynovel/Dum01Application.java
@@ -0,0 +1,13 @@
+package com.practice.mynovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dum01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dum01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/mynovel/Dum01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mynovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dum01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
